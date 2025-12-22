8b27744e23567b4eec6b14120ea779fe0c36647f
bala-amypo <bala@amypo.in>
2025-12-22 02:28:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/RoiReportRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.RoiReport;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoiReportRepository extends JpaRepository<RoiReport, Long> {
+
+    List<RoiReport> findByDiscountCode_Influencer_Id(Long influencerId);
+
+    List<RoiReport> findByDiscountCode_Campaign_Id(Long campaignId);
+}
---
+++ src/main/java/com/example/demo/repository/SaleTransactionRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.SaleTransaction;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleTransactionRepository extends JpaRepository<SaleTransaction, Long> {
+
+    List<SaleTransaction> findByDiscountCode_Id(Long discountCodeId);
+
+    List<SaleTransaction> findByDiscountCode_Influencer_Id(Long influencerId);
+
+    List<SaleTransaction> findByDiscountCode_Campaign_Id(Long campaignId);
+}
