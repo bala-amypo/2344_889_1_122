a28c211a0aaea2b81a3aae1844fccb25bf732185
bala-amypo <bala@amypo.in>
2025-12-26 17:31:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/SaleTransactionRepository.java
+++ src/main/java/com/example/demo/repository/SaleTransactionRepository.java
@@ -1,14 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.SaleTransaction;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface SaleTransactionRepository extends JpaRepository<SaleTransaction, Long> {
-
-    List<SaleTransaction> findByDiscountCode_Id(Long discountCodeId);
-
-    List<SaleTransaction> findByDiscountCode_Influencer_Id(Long influencerId);
-
-    List<SaleTransaction> findByDiscountCode_Campaign_Id(Long campaignId);
+    List<SaleTransaction> findByDiscountCodeId(Long discountCodeId);
+    List<SaleTransaction> findByDiscountCodeInfluencerId(Long influencerId);
+    List<SaleTransaction> findByDiscountCodeCampaignId(Long campaignId);
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,12 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     boolean existsByEmail(String email);
-
     Optional<User> findByEmail(String email);
+    List<User> findByRole(User.Role role);
 }
